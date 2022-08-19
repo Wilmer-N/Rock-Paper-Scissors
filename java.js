3c56d6695fa3f7f753de3f50ff10702c57f3abdf
Wilmer <wilmernorling@gmail.com>
2022-08-19 16:35:38 +0200

    I added score counter

--- java.js
+++ java.js
@@ -1,9 +1,14 @@
+let playerScore = 0;
+let computerScore = 0;
 
 function game(){
     for (let i = 0; i < 5; i++) {
+        
 function randomNumbers(min, max){
 	return Math.round(Math.random() * (max - min)) + min;
 }
+
+
 
 function getComputerChoice(choice){
     if (choice === 1){
@@ -18,18 +23,24 @@ function getComputerChoice(choice){
 
 function playRound (playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        return (`${playerSelection} vs ${computerSelection} is a draw`)
+        return (`${playerSelection} vs ${computerSelection} is a draw`);
     }else if (playerSelection === "rock" && computerSelection === "paper"){
-        return (`u loose! ${computerSelection} beats ${playerSelection}`)
+        computerScore += 1;
+        return (`u loose! ${computerSelection} beats ${playerSelection}`);
     }else if(playerSelection === "paper" && computerSelection === "scissors"){
+        computerScore += 1;
         return (`u loose! ${computerSelection} beats ${playerSelection}`);
     }else if(playerSelection === "scissors" && computerSelection === "rock"){
+        computerScore += 1;
         return (`u loose! ${computerSelection} beats ${playerSelection}`);
     }else if(playerSelection === "scissors" && computerSelection === "paper"){
+        playerScore += 1;
         return (`You win! ${playerSelection} beats ${computerSelection}`);
     }else if(playerSelection === "paper" && computerSelection === "rock"){
+        playerScore += 1;
         return (`You win! ${playerSelection} beats ${computerSelection}`);
     }else if(playerSelection === "rock" && computerSelection === "scissors"){
+        playerScore += 1;
         return (`You win! ${playerSelection} beats ${computerSelection}`);
     }else{
         return ("invalid input")
@@ -39,6 +50,8 @@ const playerSelection = prompt("choose").toLowerCase()
 const computerSelection = getComputerChoice(randomNumbers(1, 3));
 playRound(playerSelection, computerSelection);
 console.log(playRound(playerSelection, computerSelection))
+console.log(`Player has: ${playerScore / 2}`)
+console.log(`Computer has: ${computerScore / 2}`)
 
 }}
 
