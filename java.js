ce66f9084f55f4f89e2c57beaa82c890eda797d3
Wilmer <wilmernorling@gmail.com>
2022-08-19 15:07:38 +0200

    I have a algoritm that choses rock paper or scissors att random

--- java.js
+++ java.js
@@ -2,4 +2,14 @@ function randomNumbers(min, max){
 	return Math.round(Math.random() * (max - min)) + min;
 }
 
-console.log(randomNumbers(1, 3))
+function getComputerChoice(choice){
+    if (choice === 1){
+        console.log("rock")
+    }else if (choice === 2){
+        console.log("paper")
+    }else {
+        console.log("scissors")
+    }
+}
+
+getComputerChoice(randomNumbers(1, 3))
