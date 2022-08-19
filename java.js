489df98eadd35224e1c1d70fbbe2bf0bdaa26bad
Wilmer <wilmernorling@gmail.com>
2022-08-19 16:09:00 +0200

    I now have a string for all possible outcomes that accept lower/uppercase add returns corrects

--- java.js
+++ java.js
@@ -1,15 +1,39 @@
+
 function randomNumbers(min, max){
 	return Math.round(Math.random() * (max - min)) + min;
 }
 
 function getComputerChoice(choice){
     if (choice === 1){
-        console.log("rock")
+        return "rock";
     }else if (choice === 2){
-        console.log("paper")
+        return "paper";
     }else {
-        console.log("scissors")
+        return "scissors";
     }
 }
 
-getComputerChoice(randomNumbers(1, 3))
+
+function playRound (playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return (`${playerSelection} vs ${computerSelection} is a draw`)
+    }else if (playerSelection === "rock" && computerSelection === "paper"){
+        return (`u loose! ${computerSelection} beats ${playerSelection}`)
+    }else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return (`u loose! ${computerSelection} beats ${playerSelection}`);
+    }else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return (`u loose! ${computerSelection} beats ${playerSelection}`);
+    }else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return (`You win! ${playerSelection} beats ${computerSelection}`);
+    }else if(playerSelection === "paper" && computerSelection === "rock"){
+        return (`You win! ${playerSelection} beats ${computerSelection}`);
+    }else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return (`You win! ${playerSelection} beats ${computerSelection}`);
+    }else{
+        return ("invalid input")
+    }
+}
+const playerSelection = prompt("choose").toLowerCase()
+const computerSelection = getComputerChoice(randomNumbers(1, 3));
+playRound(playerSelection, computerSelection);
+console.log(playRound(playerSelection, computerSelection))
