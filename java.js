d4b6ce31849c83341f917ba84ad6e6dfaa76114d
Wilmer <wilmernorling@gmail.com>
2022-08-27 22:05:33 +0200

    sad

--- java.js
+++ java.js
@@ -1,70 +1,76 @@
 let playerScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll("button")
 
 
 game()
-
 function game(){
-    for (let i = 0; i < 5; i++) {
-
-//This section takes in to values (min, max) and puts out a random value inbetween and including them//
-function randomNumbers(min, max){
-	return Math.round(Math.random() * (max - min)) + min;
-}
-
-
-//uses the the random number from randomNumbers and converts it to a string and stores that in the const computerSelection
-function getComputerChoice(choice){
-    if (choice === 1){
-        return "rock";
-    }else if (choice === 2){
-        return "paper";
-    }else {
-        return "scissors";
+function playerChoice (e){
+    function randomNumbers(min, max){
+        return Math.round(Math.random() * (max - min) + min);
     }
+    function getComputerChoice(choice){
+        if (choice === 1){
+            return "rock";
+        }else if (choice === 2){
+            return "paper";
+        }else {
+            return "scissors";
+        }
+    }
+    let computer = getComputerChoice(randomNumbers(1, 3))
+    let player = e.target.id
+    function playRound(playerSelection, computerSelection){
+        if (playerSelection === computerSelection){
+            console.log(`${playerSelection} vs ${computerSelection} is a draw`);
+        }else if (playerSelection === "rock" && computerSelection === "paper"){
+            computerScore += 1;
+            console.log(`u loose! ${computerSelection} beats ${playerSelection}`);
+        }else if(playerSelection === "paper" && computerSelection === "scissors"){
+            computerScore += 1;
+            console.log(`u loose! ${computerSelection} beats ${playerSelection}`);
+        }else if(playerSelection === "scissors" && computerSelection === "rock"){
+            computerScore += 1;
+            console.log(`u loose! ${computerSelection} beats ${playerSelection}`);
+        }else if(playerSelection === "scissors" && computerSelection === "paper"){
+            playerScore += 1;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        }else if(playerSelection === "paper" && computerSelection === "rock"){
+            playerScore += 1;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        }else if(playerSelection === "rock" && computerSelection === "scissors"){
+            playerScore += 1;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        }else{
+            console.log("invalid input")
+        }
+        const computerScoreText = document.querySelector("#computerScore")
+        const playerScoreText = document.querySelector("#playerScore")
+        computerScoreText.textContent = `Computer has: ${computerScore}`
+        playerScoreText.textContent = `Player has: ${playerScore}`
+        
+    }
+    if (computerScore == 5 || playerScore == 5){
+        const won = document.querySelector("#won")
+        if(computerScore == 5){
+            won.textContent = "Computer won"
+        }else{
+            won.textContent = "Player won"
+        }return
+    }
+    playRound(player, computer)
 }
-const computerSelection = getComputerChoice(randomNumbers(1, 3));
 
-// makes the player enter ther input and makes it all lowercase
-const playerSelection = prompt("choose").toLowerCase()
+buttons.forEach(btn => {
+    btn.addEventListener("click", playerChoice)
+});
+
 
 //the logig behind what will get loged after each input and who wil get the point
-function playRound (playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-        return (`${playerSelection} vs ${computerSelection} is a draw`);
-    }else if (playerSelection === "rock" && computerSelection === "paper"){
-        computerScore += 1;
-        return (`u loose! ${computerSelection} beats ${playerSelection}`);
-    }else if(playerSelection === "paper" && computerSelection === "scissors"){
-        computerScore += 1;
-        return (`u loose! ${computerSelection} beats ${playerSelection}`);
-    }else if(playerSelection === "scissors" && computerSelection === "rock"){
-        computerScore += 1;
-        return (`u loose! ${computerSelection} beats ${playerSelection}`);
-    }else if(playerSelection === "scissors" && computerSelection === "paper"){
-        playerScore += 1;
-        return (`You win! ${playerSelection} beats ${computerSelection}`);
-    }else if(playerSelection === "paper" && computerSelection === "rock"){
-        playerScore += 1;
-        return (`You win! ${playerSelection} beats ${computerSelection}`);
-    }else if(playerSelection === "rock" && computerSelection === "scissors"){
-        playerScore += 1;
-        return (`You win! ${playerSelection} beats ${computerSelection}`);
-    }else{
-        return ("invalid input")
-    }
-}
 
 
-
-
-
-playRound(playerSelection, computerSelection);
-console.log(playRound(playerSelection, computerSelection))
 
 // logs the score
-console.log(`Player has: ${playerScore / 2}`)
-console.log(`Computer has: ${computerScore / 2}`)
 
-}}
 
+}
